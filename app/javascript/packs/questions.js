ea5c44fbc7434fc1adaf8d524e076f2f9b98226f
雁鸣 <cisolarix@gmail.com>
2017-11-08 11:31:54 +0800

    样式放到 layout 中去

--- app/javascript/packs/questions.js
+++ app/javascript/packs/questions.js
@@ -1,7 +1,6 @@
 import Vue from 'vue/dist/vue.esm'
 import store from '../store'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import Questions from '../components/Questions.vue'
 
 Vue.use(ElementUI)
