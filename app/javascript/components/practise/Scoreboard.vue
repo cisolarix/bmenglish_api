7bfc4d6ec2b10462a2a0ebd7bb5cc49a96bff630
雁鸣 <cisolarix@gmail.com>
2017-11-29 15:36:01 +0800

    minor change

--- app/javascript/components/practise/Scoreboard.vue
+++ app/javascript/components/practise/Scoreboard.vue
@@ -1,12 +1,10 @@
 <template lang="html">
   <div id="scoreboard" class="fixed">
-    <div>
-      <strong>答题板</strong>
-      <div>
-        <span class="chip" :class="[finished.includes(index) ? 'answered' : '']" v-for="(question, index) in questions">
-          <a :href="`#question_${index+1}`">{{ index + 1 }}</a>
-        </span>
-      </div>
+    <strong>答题板</strong>
+    <div id='numbers'>
+      <span class="chip" :class="[finished.includes(index) ? 'answered' : '']" v-for="(question, index) in questions">
+        <a :href="`#question_${index+1}`">{{ index + 1 }}</a>
+      </span>
     </div>
     <button type="button" name="action" id="submit" class="btn" :class="[finished.length == questionsLength ? '' : 'disabled']" @click="onClick">
       交卷
@@ -23,7 +21,6 @@ export default {
   },
   methods: {
     onClick() {
-      console.log("交卷")
       this.$store.dispatch('submitPractice')
     }
   }
