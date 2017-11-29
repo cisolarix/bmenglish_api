806ce22ef3a8bf9db2bc91756a75ce3bb74f297e
雁鸣 <cisolarix@gmail.com>
2017-11-29 16:52:28 +0800

    去除冗余代码

--- app/models/workbooks/drill.rb
+++
@@ -1,7 +0,0 @@
-class Workbooks::Drill < ApplicationRecord
-  self.table_name = 'workbook_drills'
-
-  belongs_to :user
-  belongs_to :workbook
-  belongs_to :less
-end
--- app/models/workbooks/drill_item.rb
+++
@@ -1,8 +0,0 @@
-class Workbooks::DrillItem < ApplicationRecord
-  self.table_name = 'workbook_drill_items'
-
-  belongs_to :user
-  belongs_to :question
-
-  serialize :selected, Array
-end
